5451cf48c246d969c62131fdb93c1dd6926a35c9
romasarca <w9tdango98osaw@proton.me>
2024-02-04 14:44:46 +0000

    v.2.5 - Added filter to the table.

--- src/app/app-encaminament.module.ts
+++ src/app/app-encaminament.module.ts
@@ -7,7 +7,7 @@ import { M03_IteradorComponent } from "./m03-iterador.component";
 import { ConctactoComponent } from "./contacto";
 
 const encaminaments: Routes = [
-  // If the path is empty, redirects to tho component salutacio.
+  // If the path is empty, redirects to index component.
   { path: "", redirectTo: "/index", pathMatch: "full" },
   { path: "index", component: IndexComponent },
   { path: "suma", component: M02_SumaComponent },
