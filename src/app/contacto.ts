2454c05a5b4939bc3eb417a679b53f39c937e101
victor23daw2 <458371.clot@fje.edu>
2024-02-05 16:32:00 +0100

    v.3 - Added contact page

--- src/app/contacto.ts
+++ src/app/contacto.ts
@@ -2,28 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'aplicacio',
-  template: `
-    <div>
-      <input type="number" placeholder="Entra número" #nombre required />
-    </div>
-
-    <button (click)="comprovarParell(nombre.value)">Comprovar</button>
-
-    <!--*ngIf is a conditional -->
-    <h2 *ngIf="esParell">És parell</h2>
-    <h3 *ngIf="!esParell">No és parell</h3>
-  `,
-  styles: ['h2 { color: #0F0 }', 'h3 {color:#900}'],
-})
-export class ConctactoComponent {
-  esParell: boolean;
-
-  constructor() {
-    this.esParell = false; // On the beggining esParell will be false.
-  }
-
-  // Function that says whether a number is odd or even.
-  comprovarParell(nombre: string): void {
-    this.esParell = parseInt(nombre) % 2 == 0; // It returns false/true.
-  }
-}
+  templateUrl: `./HTML/contacto.html`,})
+export class ContactoComponent {
+  // It returns false/true.
+  } 
