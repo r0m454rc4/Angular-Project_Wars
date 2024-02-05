0b1ea87ed9f427afb69bd8aceb66cb7bb7641808
romasarca <w9tdango98osaw@proton.me>
2024-02-05 23:46:46 +0000

    v.6 - Custom pipes created 🙃.

---
+++ src/app/campos-vacios.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CamposVaciosPipe } from './campos-vacios.pipe';
+
+describe('CamposVaciosPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CamposVaciosPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
