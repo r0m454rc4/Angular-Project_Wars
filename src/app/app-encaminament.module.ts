7f2e1a76408aa84679bb251b6ea96743da697a6c
[victor] <[victortoroarias@gmail.com]>
2024-02-11 13:20:18 +0100

    v.8 - Us page added

--- src/app/app-encaminament.module.ts
+++ src/app/app-encaminament.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
 import { IndexComponent } from "./index";
-import { M02_SumaComponent } from "./m02-suma.component";
+import { nosotrosComponent } from "./nosotrosComponent";
 import { M03_IteradorComponent } from "./m03-iterador.component";
 import { ContactoComponent } from "./contacto";
 
@@ -10,7 +10,7 @@ const encaminaments: Routes = [
   // If the path is empty, redirects to index component.
   { path: "", redirectTo: "/index", pathMatch: "full" },
   { path: "index", component: IndexComponent },
-  { path: "suma", component: M02_SumaComponent },
+  { path: "suma", component: nosotrosComponent },
   { path: "iterador", component: M03_IteradorComponent },
   { path: "contacto", component: ContactoComponent },
 ];
