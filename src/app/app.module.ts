042a3319a8d94fc98ae373bb242b7abf54f83cda
romasarca <w9tdango98osaw@proton.me>
2024-02-14 23:44:29 +0000

    v.11 - Rolling release.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,18 @@ import { MatTabsModule } from "@angular/material/tabs";
 import { MatPaginator, MatPaginatorModule } from "@angular/material/paginator";
 import { MatToolbarModule } from "@angular/material/toolbar";
 
+import { CdkAccordionModule } from "@angular/cdk/accordion";
+
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatStepperModule } from "@angular/material/stepper";
+
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatCardModule } from "@angular/material/card";
+
 import { AppEncaminamentModule } from "./app-encaminament.module";
 
 import { nosotrosComponent } from "./nosotrosComponent";
@@ -14,20 +26,6 @@ import { ContactoComponent } from "./contacto";
 import { M11_EncaminamentComponent } from "./m11-encaminament.component";
 import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
 
-
-import {CdkAccordionModule} from '@angular/cdk/accordion';
-
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {MatStepperModule} from '@angular/material/stepper';
-
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatCardModule} from '@angular/material/card';
-
-
 @NgModule({
   declarations: [
     nosotrosComponent,
@@ -35,11 +33,11 @@ import {MatCardModule} from '@angular/material/card';
     ContactoComponent,
     M11_EncaminamentComponent,
     ContactoComponent,
-    nosotrosComponent
+    nosotrosComponent,
   ], // Here I must say the components I want to be able to use.
   imports: [
     AppEncaminamentModule,
-    
+
     BrowserModule,
     BrowserAnimationsModule,
 
@@ -60,9 +58,8 @@ import {MatCardModule} from '@angular/material/card';
     MatIconModule,
     MatDividerModule,
     MatCardModule,
-
   ],
-  bootstrap: [M11_EncaminamentComponent],
-  providers: [provideAnimationsAsync()], // This is the component I'm using.
+  bootstrap: [M11_EncaminamentComponent], // This is the component I'm using.
+  providers: [provideAnimationsAsync()],
 })
 export class AppModule {}
--- src/app/contacto.ts
+++ src/app/contacto.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'aplicacio',
+  selector: "aplicacio",
   templateUrl: `./HTML/contacto.html`,
   styleUrls: ["./CSS/Style.css"],
 })
 export class ContactoComponent {
   // It returns false/true.
-  } 
+}
